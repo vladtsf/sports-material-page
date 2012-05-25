6ed9b8cf2cdbc19c43d791715c745a694f0bb292
Vladimir Tsvang <vtsvang@gmail.com>
2012-05-25 20:32:52 +0400

    Header

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,9 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' })
 };
+
+exports.blogEntry = function(req, res){
+
+	console.time('render')
+	res.render('blog-entry', require('../jsons/blog-entry.json'));
+};
